36ed857ec515e17a0d0c234c80110094382a45bb
Dylan Reed <dylan.reeder5@gmail.com>
2024-06-11 09:03:38 -0400

    Initial Commit

---
+++ Pose/PoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoseApp.swift
+//  Pose
+//
+//  Created by Dylan Reed on 2024-06-11.
+//
+
+import SwiftUI
+
+@main
+struct PoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
